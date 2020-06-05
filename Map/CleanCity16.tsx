24199977cb6984bc2eb8c83916a64df839997527
Karl Runge <karl.runge@defendtex.com>
2020-06-05 11:39:25 +1000

    added map

---
+++ Map/CleanCity16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="CleanCity16" tilewidth="16" tileheight="16" tilecount="600" columns="30">
+ <image source="cleancitygameassets_windows/Tilemap/Tilemap.png" width="480" height="320"/>
+</tileset>
---
+++ Map/OsakaCity.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="OsakaCity" tilewidth="32" tileheight="32" tilecount="357" columns="17">
+ <image source="osakacitygameassets_windows/Tilemap/Tilemap.png" width="544" height="672"/>
+</tileset>
